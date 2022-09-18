86d197b823fb374dc078bd062cfc821dce9826cc
Samokhin Alexander <icorwin74@gmail.com>
2022-09-18 21:05:40 +0300

    Separate q&a

--- read_file.py
+++ read_file.py
@@ -1,6 +1,12 @@
 import os
 
 files = os.listdir(path='questions')
-with open(f'questions/{files[0]}', "r", encoding='KOI8-R') as f:
-    file_contents = f.read()
-print(file_contents)
+with open(f'questions/{files[3]}', "r", encoding='KOI8-R') as f:
+    content = f.read().split('\n\n')
+    for paragraph in content:
+        if paragraph.startswith('Вопрос'):
+            print(" ".join(paragraph.split("\n")[1:]), '\n')
+            continue
+        if paragraph.startswith('Ответ'):
+            print(paragraph.split("\n")[1])
+            print(10*'=')
