1831386363027d404772642f2fc6b23613d2e21b
Samokhin Alexander <icorwin74@gmail.com>
2022-10-12 20:33:22 +0300

    Setup echobot

---
+++ tlgm_vopros.py
@@ -0,0 +1,61 @@
+import logging
+from environs import Env
+
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from tlgm_logger import TlgmLogsHandler
+
+
+logger = logging.getLogger(__file__)
+
+
+def echo(update, context):
+    update.message.reply_text(update.message.text)
+
+
+def start(update, context):
+    user = update.effective_user
+    update.message.reply_markdown_v2(
+        fr'Здравствуйте {user.mention_markdown_v2()}\!',
+    )
+
+
+def error_handler(update, context):
+    logger.exception('Exception', exc_info=context.error)
+
+
+def main():
+    logging.basicConfig(
+                format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                level=logging.INFO
+    )
+
+    env = Env()
+    env.read_env()
+    tlgm_token_bot = env('TLGM_TOKEN_BOT')
+
+    updater = Updater(tlgm_token_bot)
+    dispatcher = updater.dispatcher
+
+    dispatcher.add_handler(CommandHandler("start", start))
+    dispatcher.add_handler(
+        MessageHandler(Filters.text & ~Filters.command, echo))
+
+    formatter = logging.Formatter(
+        '%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+        '%H:%M:%S',
+    )
+    logger.setLevel(logging.INFO)
+    logger.addHandler(TlgmLogsHandler(
+                                      updater.bot,
+                                      env('ADMIN_TLGM_CHAT_ID'),
+                                      formatter
+                                     )
+                      )
+    dispatcher.add_error_handler(error_handler)
+
+    updater.start_polling()
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
