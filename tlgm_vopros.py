94fd5cec37f414b6b2466a72d45a8c7c8f7ecbb3
Samokhin Alexander <icorwin74@gmail.com>
2022-10-13 22:17:48 +0300

    Add conservation handler

--- tlgm_vopros.py
+++ tlgm_vopros.py
@@ -1,24 +1,28 @@
-import os
 import logging
-import redis
+import os
 from random import randint
-from environs import Env
 
+import redis
+from environs import Env
 from telegram import ReplyKeyboardMarkup
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from telegram.ext import (CommandHandler, Filters, MessageHandler,
+                             Updater, ConversationHandler)
+
 from tlgm_logger import TlgmLogsHandler
 
+QUESTION = 1
+ANSWER = 2
+HINT = 3
 
 logger = logging.getLogger(__file__)
 
 
 def echo(update, context):
-    if update.message.text == 'Новый вопрос':
-        quiz_id = randint(0, context.bot_data['max_quiz_id'])
-        redis = context.bot_data['redis']
-        redis.set(1, quiz_id)
-        update.message.reply_text(
-            context.bot_data['quiz'][quiz_id][0])
+    redis = context.bot_data['redis']
+    question_id = int(redis.get(update.message.chat.id))
+    print(context.bot_data['quiz'][question_id][1])
+    print(update.message.text)
+
 
 
 def start(update, context):
@@ -29,7 +33,41 @@ def start(update, context):
         fr'Здравствуйте {user.mention_markdown_v2()}\! Я бот для викторин\!',
         reply_markup=reply_markup,
     )
+    return QUESTION
 
+def question(update, context):
+    user = update.effective_user
+    custom_keyboard = [['Новый вопрос', 'Сдаться'],['Мой счет']]
+    reply_markup = ReplyKeyboardMarkup(custom_keyboard)
+    redis = context.bot_data['redis']
+    quiz_id = randint(0, context.bot_data['max_quiz_id'])
+    redis.set(update.message.chat.id, quiz_id)
+    update.message.reply_text(
+            context.bot_data['quiz'][quiz_id][0],
+            reply_markup=reply_markup)
+    update.message.reply_text(
+        context.bot_data['quiz'][quiz_id][1],
+        reply_markup=reply_markup)
+    return ANSWER
+
+def answer(update, context):
+    redis = context.bot_data['redis']
+    question_id = int(redis.get(update.message.chat.id))
+    if update.message.text.lower() == \
+        context.bot_data['quiz'][question_id][1].strip('\"').split('.')[0].lower():
+        custom_keyboard = [['Новый вопрос', 'Сдаться'], ['Мой счет']]
+        reply_markup = ReplyKeyboardMarkup(custom_keyboard)
+        update.message.reply_text(
+            'Правильно! Поздравляю! Для следующего вопроса нажми «Новый вопрос»',
+            reply_markup=reply_markup,
+        )
+        return QUESTION
+    update.message.reply_text('Неправильно… Попробуешь ещё раз?')
+    return ANSWER
+
+def cancel(update, context):
+    update.message.reply_text('Галя, Отмена!')
+    return QUESTION
 
 def error_handler(update, context):
     logger.exception('Exception', exc_info=context.error)
@@ -41,7 +79,13 @@ def main():
                 level=logging.INFO
     )
 
-    redis_db = redis.Redis(host='localhost', port=6379, db=0)
+    redis_db = redis.Redis(
+                           host='localhost',
+                           port=6379,
+                           db=0,
+                           charset="utf-8",
+                           decode_responses=True
+    )
 
     files = os.listdir(path='questions')
     quiz = []
@@ -64,9 +108,26 @@ def main():
     dispatcher.bot_data['max_quiz_id'] = len(quiz) - 1
     dispatcher.bot_data['redis'] = redis_db
 
-    dispatcher.add_handler(CommandHandler("start", start))
-    dispatcher.add_handler(
-        MessageHandler(Filters.text & ~Filters.command, echo))
+    conv_handler = ConversationHandler(
+        entry_points=[CommandHandler('start', start)],
+
+        states={
+            QUESTION: [
+                       MessageHandler(Filters.regex(r'^(Новый вопрос)$'), question),
+                       ],
+
+            ANSWER: [
+                      MessageHandler(Filters.regex(r'^(Сдаться)$'), cancel),
+                      MessageHandler(Filters.text & ~Filters.command, answer)
+                    ],
+            HINT: []
+        },
+
+        fallbacks=[CommandHandler('cancel', cancel)]
+    )
+
+
+    dispatcher.add_handler(conv_handler)
 
     formatter = logging.Formatter(
         '%(asctime)s - %(name)s - %(levelname)s - %(message)s',
